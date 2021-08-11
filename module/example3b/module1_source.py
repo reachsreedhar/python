e956975a5446814ffce2d821a80a146e8f29c531
Sreedhar Chidambaram <sreedhar@capterra.com>
2021-08-11 11:52:23 -0400

    more examples

---
+++ module/example3b/module1_source.py
@@ -0,0 +1,4 @@
+print('Running module1_source.py')
+
+def hello():
+  print("module says hello")
---
+++ module/example3b/module2.py
@@ -0,0 +1,6 @@
+print('Running module2.py')
+import module1
+
+def hello():
+  print('module2 syas Hello!\nand ...')
+  module1.hello()
